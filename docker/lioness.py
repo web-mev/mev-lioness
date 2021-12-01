8efd23a1006834fbf2ec3ca8efecbe975a0265e2
dkdeconti <deconti.d@gmail.com>
2021-12-01 13:14:15 -0500

    Added initial LIONESS.

---
+++ docker/lioness.py
@@ -0,0 +1,74 @@
+# /usr/bin/env python
+
+import argparse
+from netZooPy.panda import Panda
+from netZooPy.lioness import Lioness
+from netZooPy.lioness.analyze_lioness import AnalyzeLioness
+import pandas as pd
+import pickle
+import sys
+
+
+def run_lioness(panda_obj, start, end):
+    '''Runs LIONESS with PANDA object.'''
+    lioness_obj = Lioness(
+        panda_obj, 
+        save_dir='../data',
+        start = start,
+        end = end
+    )
+    output_filename = "lioness_matrix.npy"
+    lioness_obj.save_lioness_results(file=output_filename)
+
+
+def load_panda_obj(panda_filename):
+    '''Loads pickle and returns object.'''
+    with open(panda_filename, 'r') as f:
+        panda_obj = pickle.load(f)
+        return panda_obj
+
+
+def parse_slices(tsv_filename, line_num):
+    '''Parses TSV for line number denoting slice ranges.'''
+    slice_ranges = []
+    with open(tsv_filename, 'r') as handle:
+        for line in handle:
+            arow = line.strip('\n').split('\t')
+            slice_ranges.append(arow)
+    return slice_ranges[line_num]
+
+
+def main():
+    # Parse args
+    parser = argparse.ArgumentParser(
+        desc="Runs LIONESS on input PANDA pickle"
+    )
+    parser.add_argument(
+        "--slices", metavar="TSV", required=True,
+        help="TSV of scatter slices"
+    )
+    parser.add_argument(
+        "--line", metavar="INT", required=True,
+        help="Line number for slice ranges"
+    )
+    parser.add_argument(
+        "panda", metavar="PICKLE",
+        help="PANDA pickle object"
+    )
+    args = parser.parse_args()
+    # Load slicing ranges
+    start, end = parse_slices(args.slices, args.line)
+    try:
+        start, end = int(start), int(end)
+    except ValueError as e:
+        sys.stderr.write("ValueError: %s\n" % str(e))
+        sys.exit()
+    # Load PANDA object
+    panda_obj = load_panda_obj(args.panda)
+    # Run LIONESS
+    run_lioness(panda_obj, start, end)
+
+
+
+if __name__ == "__main__":
+    main()
--- main.wdl
+++ main.wdl
@@ -7,11 +7,24 @@ workflow mevLioness {
     # A user uploaded exprs count matrix
     File exprs_file
 
+    # Number of scatters to split the samples over
+    Int num_ranges = 10
+
     call runPandas {
         input:
             motif_file = motif_file,
             ppi_file = ppi_file,
-            exprs_file = exprs_file
+            exprs_file = exprs_file,
+            num_ranges = num_ranges
+    }
+
+    scatter (line_num in range(num_ranges)) {
+        call runLioness {
+            input:
+                panda_pickle = runPandas.panda_pickle,
+                slice_tsv = runPandas.scatter_ranges,
+                line_num = line_num
+        }
     }
 
 }
@@ -34,6 +47,33 @@ task runPandas {
         File panda_pickle = "panda_obj.pkl"
         File panda_matrix = "panda_output.mtx"
         File scatter_ranges = "sample_scatter_ranges.txt"
+    }
+
+    runtime {
+        docker: "hsphqbrc/mev-netzoopy"
+        cpu: 8
+        memory: "128 G"
+        disks: "local-disk " + disk_size + " HDD"
+        preemptible: 0
+    }
+}
+
+task runLioness {
+    File panda_pickle
+    File slice_tsv
+    Int line_num
+    
+    Int disk_size = 40
+
+    command {
+        python3 /opt/software/lioness.py \
+            --slices ${slice_tsv} \
+            --line ${line_num} \
+            ${panda_pickle}
+    }
+
+    output {
+        File lioness_matrix = "lioness_matrix.npy"
     }
 
     runtime {
