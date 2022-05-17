29a2191d8a38e79d9fbbd07aa6e372a6c2a400ef
dkdeconti <deconti.d@gmail.com>
2022-05-16 23:44:47 -0400

    Added docstrings and changed PANDA matrix output.

--- docker/panda.py
+++ docker/panda.py
@@ -8,16 +8,20 @@ import pickle
 import sys
 
 
-def slice_matrix(exprs_filename, num_ranges):
-    '''
-    Slices matrix into scatter chunks and exports slices to file.
-    '''
+def slice_matrix(exprs_filename, output_filename, num_ranges):
+    """Slices samples into bins for LIONESS scatter.
+
+    Args:
+        exprs_filename (str): file name for input expression matrix
+        output_filename (str): output file name for scatter slices
+        num_ranges (int): number of ranges to evenly slice samples by
+    """
     try:
         num_ranges = int(num_ranges)
     except ValueError as e:
         sys.stderr.write("ValueError: %s\n" % str(e))
         sys.exit()
-    exprs_df = pd.read_csv(exprs_filename, index_col = 0, header = 0)
+    exprs_df = pd.read_csv(exprs_filename, index_col = 0, header = 0, sep="\t")
     sample_size = exprs_df.shape[0]
     range_list = []
     quantile = int(sample_size / num_ranges)
@@ -28,15 +32,18 @@ def slice_matrix(exprs_filename, num_ranges):
             end = sample_size + 1 # avoids division remainder issues
     range_list.append((start, end))
     # Export ranges to file as TSV
-    with open("sample_scatter_ranges.txt", 'w') as handle:
+    with open(output_filename, 'w') as handle:
         for start, end in range_list:
             handle.write("%i\t%i\n" % (start, end))
 
 
 def run_panda(args, panda_output):
-    '''
-    Runs PANDA object creation and exports output to file and pickle.
-    '''
+    """Run PANDA and saves as pickle.
+
+    Args:
+        args (argparse.ArgumentParser()): passed args from main()
+        panda_output (str): output file name for PANDA TSV
+    """    
     # Load the data as a pandas dataframes
     # Required for LIONESS
     exprs_df = pd.read_csv(args.exprs, index_col = 0, header = 0)
@@ -55,20 +62,37 @@ def run_panda(args, panda_output):
         exprs_df,
         motif_df,
         ppi_df,
-        save_tmp=True,
-        save_memory=True,
-        remove_missing=False,
-        keep_expression_matrix=False
+        remove_missing=False, 
+        keep_expression_matrix=True, 
+        save_memory=False, 
+        modeProcess='legacy'
     )
-    panda_obj.save_panda_results(panda_output)
+    # Save panda output as bi-partite matrix TSV
+    df = panda_obj.export_panda_results
+    tfs = df['tf'].drop_duplicates()
+    genes = df['gene'].drop_duplicates()
+    arr = np.array(df['force']).reshape(len(genes), len(tfs))
+    pd.DataFrame(
+        arr,
+        index=genes,
+        columns=tfs
+    ).to_csv(panda_output, sep="\t")
+    # Save PANDA object as pickle
     with open("panda_obj.pkl", "wb") as f:
         pickle.dump(panda_obj, f)
 
 
 def main():
+    """Runs PANDA on input expression matrix and produces scatters.
+    """    
     # Parse args
     parser = argparse.ArgumentParser(
         desc="Runs PANDA on input count matrix."
+    )
+    parser.add_argument(
+        "-s", "--scatter", metavar="STR", required=True,
+        default="sample_scatter_ranges.txt",
+        help="output name for scatter ranges"
     )
     parser.add_argument(
         "--ranges", metavar="INT", required=True,
@@ -83,14 +107,17 @@ def main():
         help="PPI data"
     )
     parser.add_argument(
+        "--out", metavar="STRING", default="panda_output.mtx",
+        help="file name for output PANDA matrix TSV"
+    )
+    parser.add_argument(
         "exprs", metavar="TSV",
         help="Expression count matrix"
     )
     args = parser.parse_args()
-    panda_output = "panda_output.mtx"
     # Run PANDA
     slice_matrix(args.exprs, args.ranges)
-    run_panda(args, panda_output)
+    run_panda(args, args.out)
 
 
 if __name__ == "__main__":
